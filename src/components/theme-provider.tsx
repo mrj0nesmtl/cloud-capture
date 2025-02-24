bd7451749069d41b49b9849841b72c494b5bf8a3
Joel <joel.yaffe@gmail.com>
2025-02-24 00:30:36 -0500

    fix(settings): update theme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,9 +2,23 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+}
+
+export function ThemeProvider({ 
+  children,
+  attribute = "class",
+  defaultTheme = "system",
+  enableSystem = true,
+  storageKey = "theme",
+  ...props 
+}: ThemeProviderProps) {
   const [mounted, setMounted] = React.useState(false)
 
   // Only run the effect on client side
@@ -18,7 +32,13 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   }
 
   return (
-    <NextThemesProvider {...props}>
+    <NextThemesProvider
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+      storageKey={storageKey}
+      {...props}
+    >
       {children}
     </NextThemesProvider>
   )
