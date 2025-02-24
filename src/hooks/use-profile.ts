d43f5ea1ce4941dc620af1af9f0a209c82e2b626
Joel <joel.yaffe@gmail.com>
2025-02-24 11:09:00 -0500

    fix(settings): update use-profiles

--- src/hooks/use-profile.ts
+++ src/hooks/use-profile.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { createClient } from '@/lib/supabase/client-config'
+import { createClient } from '@/lib/supabase/client'
 import type { Database } from '@/types/supabase'
 
 type Profile = Database['public']['Tables']['profiles']['Row']
